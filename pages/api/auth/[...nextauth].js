54575a7c6e84e72c42b57118b295f0046e4d1052
jyotish4017 <jyotish4017@gmail.com>
2022-03-24 17:42:20 +0530

    secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -19,6 +19,7 @@ export default NextAuth({
             .toLocaleLowerCase();
             session.user.uid = token.sub;
             return session;
-      }
-  }
+      },
+  },
+  secret: process.env.JWT_SECRET, 
 });
